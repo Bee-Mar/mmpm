90cea81bf5f686fce4e84fe7522a041177a4893a
Brandon Marlowe <bpmarlowe-software@protonmail.com>
2023-01-22 21:18:52 -0500

    updated docker versions for node and python

--- scripts/ci/compress-static-files.sh
+++ scripts/ci/compress-static-files.sh
@@ -5,6 +5,6 @@ VERSION=$(git describe --tags --abbrev=0)
 cd gui/build
 mkdir -p templates
 cp static/index.html templates
-tar czvf mmpm-gui-$VERSION.tar.gz * && mv *gz ../..
+tar czf mmpm-gui-$VERSION.tar.gz * && mv *gz ../..
 cp -r {static,templates} ../../mmpm
 cd ../../

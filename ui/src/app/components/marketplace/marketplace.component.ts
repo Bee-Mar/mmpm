daacf129667802c83e3515e352461dbdfa39de33
Brandon Marlowe <brandon.marlowe@radiancetech.com>
2023-12-12 10:35:29 -0500

    removed unused var

--- ui/src/app/components/marketplace/marketplace.component.ts
+++ ui/src/app/components/marketplace/marketplace.component.ts
@@ -13,18 +13,17 @@ export class MarketPlaceComponent implements OnInit, OnDestroy {
   constructor(private store: SharedStoreService) {}
 
   private packagesSubscription: Subscription = new Subscription();
-  private defaultIcon = DefaultMarketPlaceIcon;
 
   public icons = MarketPlaceIcons;
   public packages = new Array<MagicMirrorPackage>();
   public categories = new Array<string>();
   public selectedPackages = new Array<MagicMirrorPackage>();
   public loading = true;
-  public readonly installedOptions = [true, false];
   public selectedInstalled: boolean | null = null;
   public selectedPackage: MagicMirrorPackage | null = null;
   public displayDetailsDialog = false;
   public selectedCategories = new Array<string>();
+  public readonly installedOptions = [true, false];
 
   public ngOnInit(): void {
     this.packagesSubscription = this.store.packages.subscribe((packages: Array<MagicMirrorPackage>) => {
@@ -32,10 +31,10 @@ export class MarketPlaceComponent implements OnInit, OnDestroy {
 
       this.categories = this.packages.map((pkg) => pkg.category).filter((category, index, self) => self.indexOf(category) === index);
 
-      // add a default icon for any category that isn't recognized above
+      // add a default icon for any category that isn't recognized
       this.packages.forEach((pkg: MagicMirrorPackage) => {
         if (pkg.category && !this.icons[pkg.category]) {
-          this.icons[pkg.category] = {...this.defaultIcon};
+          this.icons[pkg.category] = {...DefaultMarketPlaceIcon};
         }
       });
 

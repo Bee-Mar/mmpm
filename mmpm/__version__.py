0df2a651f7c4012e7904576495842892c29f9aa1
Brandon Marlowe <bpmarlowe-software@protonmail.com>
2023-12-26 14:52:17 -0500

    silly mistake not calling get on the env vars

--- mmpm/__version__.py
+++ mmpm/__version__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 major = 4
 minor = 0
-patch = 3
+patch = 4
 
 version = f"{major}.{minor}.{patch}"
--- mmpm/api/endpoints/ep_mmpm.py
+++ mmpm/api/endpoints/ep_mmpm.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-from flask import Blueprint, Response
-
 import mmpm.__version__
 import mmpm.utils
+from flask import Blueprint, Response
 from mmpm.api.constants import http
 from mmpm.api.endpoints.endpoint import Endpoint
 from mmpm.env import MMPMEnv
@@ -45,7 +44,7 @@ class Mmpm(Endpoint):
             Returns:
                 Response: A Flask Response object indicating success or failure of the upgrade operation.
             """
-            if self.env.MMPM_IS_DOCKER_IMAGE:
+            if self.env.MMPM_IS_DOCKER_IMAGE.get():
                 self.failure("MMPM is a Docker image. MMPM must be upgraded using the Docker CLI.")
             if mmpm.utils.upgrade():
                 return self.success("Upgrade MMPM")

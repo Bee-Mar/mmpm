0fb03198e1f18b1bf7bebab656225e332ad3cf3d
Brandon Marlowe <bpmarlowe-software@protonmail.com>
2025-03-09 10:49:29 -0400

    testing custom build hook

--- .build.py
+++ .build.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import sys
 from os import chdir, getenv, system
 from pathlib import Path
-from shutil import copytree
+from shutil import copytree, which
 
 
 def build_ui():
-    print(f"[+] Running custom build hook in {Path(__file__)}")
+    print(f"  - Running custom build hook in {Path(__file__)}")
 
     # if the DEVBOX_PROJECT_ROOT exists, it'll be returned as a string, make sure it's a Path object
     root = Path(getenv("DEVBOX_PROJECT_ROOT", Path.cwd()))
@@ -13,12 +14,18 @@ def build_ui():
     Path(root / "mmpm" / "ui").mkdir(exist_ok=True, parents=True)
     chdir(root / "ui")
 
-    system("bun install --legacy-peer-deps")
+    if not which("bun") and not which("npm"):
+        print("ERROR: both `npm` and `bun` are not installed. Cannot continue")
+        sys.exit(1)
+
+    node = which("bun") or "npm"
+
+    system(f"{node} install --legacy-peer-deps")
     system("./node_modules/@angular/cli/bin/ng.js build --configuration production --output-hashing none --base-href /")
 
     chdir(root)
 
-    copytree(root / "ui" / "build", root / "mmpm" / "ui", dirs_exist_ok=True)
+    copytree(root / "ui" / "build" / "browser", root / "mmpm" / "ui", dirs_exist_ok=True)
 
 
 if __name__ == "__main__":
